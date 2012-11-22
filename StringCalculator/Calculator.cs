d39ed3527f2935f058480715738b36e1d1292a84
Ibai Imaz <ibaiimaz@gmail.com>
2012-11-22 22:24:06 +0100

    Complete code of String Calculatro Kata

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string numbersString)
+        {
+            if (numbersString == string.Empty) return 0;
+
+            return GetNumbers(numbersString).Sum();
+        }
+
+        public List<int> GetNumbers(string numbersString)
+        {
+            string commaDelimitedNumbers = GetCommaDelimitedNumbers(numbersString);
+
+            var numbers = commaDelimitedNumbers.Split(',').Select(int.Parse);
+
+            ValidateNegativeNumbers(numbers);
+
+            return IgnoreNumbersBiggerThan1000(numbers);
+        }
+
+        private string GetCommaDelimitedNumbers(string numbers)
+        {
+            if (HasCustomDelimiters(numbers))
+            {
+                var delimiters = GetDelimiters(numbers);
+                numbers = GetNumbersStringWithoutCustomDelimiters(numbers);
+
+                numbers = ReplaceAllCustomDelimitersWithComma(numbers, delimiters);
+            }
+
+            return numbers.Replace(@"\n", ",");
+        }
+
+        private string ReplaceAllCustomDelimitersWithComma(string numbers, List<string> delimiters)
+        {
+            return delimiters.Aggregate(numbers, (current, delimiter) => current.Replace(delimiter, ","));
+        }
+
+        private string GetNumbersStringWithoutCustomDelimiters(string numbers)
+        {
+            return numbers.Substring(numbers.IndexOf(@"\n") + 2);
+        }
+
+        private bool HasCustomDelimiters(string numbers)
+        {
+            return numbers.StartsWith("//");
+        }
+
+        private List<string> GetDelimiters(string numbers)
+        {
+            string delimitersString = GetCustomDelimitersString(numbers);
+
+            if (ContainsDelimitersWithBrackets(delimitersString))
+            {
+                var delimiters = GetDelimitersListFromDelimitersString(delimitersString);
+
+                return DiscardBlankItems(delimiters);
+            }
+            return new List<string> {delimitersString};
+        }
+
+        private List<string> DiscardBlankItems(List<string> delimiters)
+        {
+            return delimiters.Where(c => c != "").ToList();
+        }
+
+        private string GetCustomDelimitersString(string numbers)
+        {
+            return numbers.Substring(2, numbers.IndexOf(@"\n") - 2);
+        }
+
+        private bool ContainsDelimitersWithBrackets(string delimitersString)
+        {
+            return delimitersString.Contains("]");
+        }
+
+        private List<string> GetDelimitersListFromDelimitersString(string delimitersString)
+        {
+            return delimitersString.Split(']').Select(c => c.Replace("[", "")).ToList();
+        }
+
+        private void ValidateNegativeNumbers(IEnumerable<int> numbers)
+        {
+            var negativeNumbers = numbers.Where(number => number < 0);
+
+            if (negativeNumbers.Any())
+                throw new ArgumentException(string.Format(
+                    "No se admiten los siguientes números por ser negativos: {0}",
+                    String.Join(",", negativeNumbers.ToList())));
+        }
+
+        private List<int> IgnoreNumbersBiggerThan1000(IEnumerable<int> numbers)
+        {
+            return numbers.Where(number => number <= 1000).ToList();
+        }
+    }
+}
---
+++ StringCalculatorTests/CalculatorTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        private Calculator calculator;
+        
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            calculator = new Calculator();
+        }
+
+        [TestMethod]
+        public void Add_NoNumber_ReturnsZero()
+        {
+            Assert.AreEqual(0, calculator.Add(""));
+        }
+
+        [TestMethod]
+        public void Add_OneNumber_ReturnsNumber()
+        {
+            Assert.AreEqual(1, calculator.Add("1"));
+        }
+
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            Assert.AreEqual(3, calculator.Add("1,2"));
+        }
+
+        [TestMethod]
+        public void Add_MultipleNumbers_ReturnsSum()
+        {
+            Assert.AreEqual(17, calculator.Add("1,2,6,8"));
+        }
+
+        [TestMethod]
+        public void Add_NewlineDelimitedNumbers_ReturnsSum()
+        {
+            Assert.AreEqual(6, calculator.Add(@"1\n2,3"));
+        }
+
+        [TestMethod]
+        public void Add_CustomDelimiterNumbers_ReturnsSum()
+        {
+            Assert.AreEqual(3, calculator.Add(@"//;\n1;2"));
+        }
+
+        [TestMethod]
+        public void Add_OneNegativeNumber_ReturnsExceptionAndShowNumberInMessage()
+        {
+            try
+            {
+                calculator.Add("-1");
+
+                Assert.Fail("This should fail as number is negative");
+            }
+            catch (ArgumentException exception)
+            {
+                // Ok, this works
+                Assert.IsTrue(exception.Message.Contains("-1"));
+            }
+        }
+
+        [TestMethod]
+        public void Add_TwoNegativeNumbers_ReturnsExceptionAndShowTwoNumbersInMessage()
+        {
+            try
+            {
+                calculator.Add("-1,-5");
+
+                Assert.Fail("This should fail as numbers are negatives");
+            }
+            catch (ArgumentException exception)
+            {
+                // Ok, this works
+                Assert.IsTrue(exception.Message.Contains("-1,-5"));
+            }
+        }
+
+        [TestMethod]
+        public void Add_NumbersBigger1000_ReturnsSumIngnoringThese()
+        {
+            Assert.AreEqual(2, calculator.Add("2,1001"));
+        }
+
+        [TestMethod]
+        public void Add_LargeDelimiterNumbers_ReturnsSum()
+        {
+            Assert.AreEqual(6, calculator.Add(@"//[***]\n1***2***3"));
+        }
+
+        [TestMethod]
+        public void Add_MultipleDemilimerNumbers_ReturnsSum()
+        {
+            Assert.AreEqual(6,calculator.Add(@"//[*][%]\n1*2%3"));
+        }
+    }
+}
